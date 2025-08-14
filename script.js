b357f4f24cdbef77a2b6a09b90a6d0d9e3bb951d
emma-nuel1708 <olanuelemmayemi@gmail.com>
2025-08-14 21:33:17 +0100

    editing

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ document.addEventListener("DOMContentLoaded", function () {
     navlinks.classList.remove("show");
   });
 });
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+function toggleMenu() {
+  const nav = document.getElementById("navlinks");
+  nav.classList.toggle("show");
+}
